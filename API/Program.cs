b914c7e54a3a01c6b5f4c373f33c857ffe56e8f7
SiddharthPatil <siddharthpatilcourses@gmail.com>
2026-01-18 22:32:49 +0530

    Angular basic structure- End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,9 +14,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 //if (app.Environment.IsDevelopment())
 //{
 //    app.MapOpenApi();
